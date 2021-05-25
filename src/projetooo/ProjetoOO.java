b56f07ceac00467d30d09592b0297f8bb01abe2a
Jeferson Leon <jefersonleonblue@gmail.com>
2021-05-25 17:28:56 -0300

    Aula 25/05/2021 - Introdução POO exemplo Conta e Carro - atualizado com menu para movimentação da conta

--- src/projetooo/ProjetoOO.java
+++ src/projetooo/ProjetoOO.java
@@ -13,13 +13,40 @@ public class ProjetoOO {
      */
     public static void main(String[] args) {
         Conta objConta = new Conta();
-        objConta.mostrarConta();
-        objConta.cadastrarConta();
         Scanner ler = new Scanner(System.in);
-        System.out.println("Quanto deseja depositar?");
-        objConta.depositar(ler.nextDouble());
-        System.out.println("Quanto deseja Sacar?");
-        objConta.sacar(ler.nextDouble());
+        int op;
+        op = 1;
+        while(op != 0 ){
+            System.out.println("BANCO VAI COM DEUS");
+            System.out.println("1 - Cadastro");
+            System.out.println("2 - Exibir Conta");
+            System.out.println("3 - Depositar");
+            System.out.println("4 - Sacar");
+            System.out.println("5 - Emitir Saldo");
+            System.out.println("0 - Sair");
+            op = ler.nextInt();
+            if(op==1){
+                objConta.cadastrarConta();
+            }else if(op==2){
+                objConta.mostrarConta();
+            }else if(op==3){
+                System.out.println("Qual o valor do Deposito?");
+                objConta.depositar(ler.nextDouble());
+            }else if(op==4){
+                System.out.println("Qual o valor do Saque?");
+                objConta.sacar(ler.nextDouble());
+            }else if(op==5){
+                objConta.emitirSaldo();
+            }else if(op==0){
+                System.out.println("Obrigado pelo uso!!");
+            }else{
+                System.out.println("Opção inválida!!");
+            }
+                    
+            
+            
+            
+        }
         
 
     }
